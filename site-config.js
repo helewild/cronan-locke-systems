bc51867b9f591836b26bad0c55df2ec97b6f6ab0
Wicked <reeves166@gmail.com>
2026-04-02 07:58:36 -0400

    Configure live Apps Script bridge URL

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 window.CRONAN_LOCKE_CONFIG = {
-  apiUrl: "",
-  siteMode: "demo"
+  apiUrl: "https://script.google.com/macros/s/AKfycbwdHTZgApmf1-thCTbTLFCQLxNK9yP-yKkAMS5lBbmmlO-SuHJRnvxLFcC2PRa23oV_IQ/exec",
+  siteMode: "live"
 };
